bf193ab605964e4f463fbf0c724ec9eef6b1623e
Pedro Pacheco <pacheckdev@gmail.com>
2021-08-04 17:18:23 -0300

    app configuration

---
+++ src/container/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import HomePage from '../pages/home-page';
+import { AppContainer } from './styles';
+function App() {
+  return (
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+  );
+}
+
+export default App;
